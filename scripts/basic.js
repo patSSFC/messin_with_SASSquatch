059378fcf4c142b191618a286ee4b60f03aa1168
Pat Yolian <pyolian@gmail.com>
2016-03-23 14:51:04 -0700

    Add responsive nav, extra config Owl Slider, small CSS edits

---
+++ scripts/basic.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    dots: false,
+    autoplay: true,
+    center: true,
+    autoplayTimeout: 700,
+    stagePadding: 10,
+    margin: 20,
+    items: 1.3,
+    }
+  );
+  $('#hamburger').on ('click', function() {
+    $('#nav-contain-test').slideToggle();
+  });
+});
